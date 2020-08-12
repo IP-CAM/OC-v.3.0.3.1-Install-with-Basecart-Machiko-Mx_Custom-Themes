282c9005b58164e66469dc740d34dd7797599759
Weslley Da Costa Hendz <58227073+weslleyHendz@users.noreply.github.com>
2020-08-12 17:26:06 -0300

    Update config

--- admin/config.php
+++ admin/config.php
@@ -1,18 +1,36 @@
 <?php
+
 // HTTP
-define('HTTP_SERVER', 'http://192.168.0.108/open/admin/');
-define('HTTP_CATALOG', 'http://192.168.0.108/open/');
+define('HTTP_SERVER', 'http://evonnis-com.umbler.net/admin/');
+define('HTTP_CATALOG', 'http://evonnis-com.umbler.net/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://192.168.0.108/open/admin/');
-define('HTTPS_CATALOG', 'http://192.168.0.108/open/');
+define('HTTPS_SERVER', 'http://evonnis-com.umbler.net/admin');
+define('HTTPS_CATALOG', 'http://evonnis-com.umbler.net/');
+
+// Máquina Local
+// // HTTP
+// define('HTTP_SERVER', 'http://192.168.0.108/open/admin/');
+// define('HTTP_CATALOG', 'http://192.168.0.108/open/');
+
+// // HTTPS
+// define('HTTPS_SERVER', 'http://192.168.0.108/open/admin/');
+// define('HTTPS_CATALOG', 'http://192.168.0.108/open/');
+
 
 // DIR
-define('DIR_APPLICATION', '/var/www/html/open/admin/');
-define('DIR_SYSTEM', '/var/www/html/open/system/');
-define('DIR_IMAGE', '/var/www/html/open/image/');
+define('DIR_APPLICATION', '/home/evonnis.com/public/admin/');
+define('DIR_SYSTEM', '/home/evonnis.com/public/system/');
+define('DIR_IMAGE', '/home/evonnis.com/public/image/');
+
+// DIR Máquina Local
+// define('DIR_APPLICATION', '/var/www/html/open/admin/');
+// define('DIR_SYSTEM', '/var/www/html/open/system/');
+// define('DIR_IMAGE', '/var/www/html/open/image/');
 define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
-define('DIR_CATALOG', '/var/www/html/open/catalog/');
+define('DIR_CATALOG', '/home/evonnis.com/public/catalog/');
+// // DIR Máquina Local
+// define('DIR_CATALOG', '/var/www/html/open/catalog/');
 define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
 define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
 define('DIR_CONFIG', DIR_SYSTEM . 'config/');
@@ -25,12 +43,21 @@ define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
-define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'hendz');
-define('DB_PASSWORD', '');
-define('DB_DATABASE', 'teste');
+define('DB_HOSTNAME', 'mysql669.umbler.com');
+define('DB_USERNAME', 'evonnis');
+define('DB_PASSWORD', 'letsdobetter');
+define('DB_DATABASE', 'opencart_test');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
+
+// DB - Máquina Local
+// define('DB_DRIVER', 'mysqli');
+// define('DB_HOSTNAME', 'localhost');
+// define('DB_USERNAME', 'hendz');
+// define('DB_PASSWORD', '');
+// define('DB_DATABASE', 'teste');
+// define('DB_PORT', '3306');
+// define('DB_PREFIX', 'oc_');
 
 // OpenCart API
 define('OPENCART_SERVER', 'https://www.opencart.com/');
--- config.php
+++ config.php
@@ -1,14 +1,30 @@
+
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://192.168.0.108/open/');
+define('HTTP_SERVER', 'http://evonnis-com.umbler.net/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://192.168.0.108/open/');
+define('HTTPS_SERVER', 'http://evonnis-com.umbler.net/');
+
+// Máquina Local
+// // HTTP
+// define('HTTP_SERVER', 'http://192.168.0.108/open/');
+
+// // HTTPS
+// define('HTTPS_SERVER', 'http://192.168.0.108/open/');
+
 
 // DIR
-define('DIR_APPLICATION', '/var/www/html/open/catalog/');
-define('DIR_SYSTEM', '/var/www/html/open/system/');
-define('DIR_IMAGE', '/var/www/html/open/image/');
+// Em produção
+define('DIR_APPLICATION', '/home/evonnis.com/public/catalog/');
+define('DIR_SYSTEM', '/home/evonnis.com/public/system/');
+define('DIR_IMAGE', '/home/evonnis.com/public/image/');
+
+
+// Máquina local
+// define('DIR_APPLICATION', '/var/www/html/open/catalog/');
+// define('DIR_SYSTEM', '/var/www/html/open/system/');
+// define('DIR_IMAGE', '/var/www/html/open/image/');
 define('DIR_STORAGE', DIR_SYSTEM . 'storage/');
 define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
 define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
@@ -22,9 +38,18 @@ define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
-define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'hendz');
-define('DB_PASSWORD', '');
-define('DB_DATABASE', 'teste');
+define('DB_HOSTNAME', 'mysql669.umbler.com');
+define('DB_USERNAME', 'evonnis');
+define('DB_PASSWORD', 'letsdobetter');
+define('DB_DATABASE', 'opencart_test');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
+
+// DB - Máquina Local
+// define('DB_DRIVER', 'mysqli');
+// define('DB_HOSTNAME', 'localhost');
+// define('DB_USERNAME', 'hendz');
+// define('DB_PASSWORD', '');
+// define('DB_DATABASE', 'teste');
+// define('DB_PORT', '3306');
+// define('DB_PREFIX', 'oc_');
